1b3a6e7e4f87546d335c6669f343b59357a39bcf
James Hawk <jph94880@gmail.com>
2023-08-09 21:53:58 -0500

    Passes seven tests

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 3, "M", "Brown", "Nice", 1);
+
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive)
+VALUES
+("Rowdy", 5, "M", "Polar", "Rowdy", 1);
+
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive)
+VALUES
+("Tabitha", 7, "F", "Black", "Friendly", 0);
+
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive)
+VALUES
+("Sergeant Brown", 30, "M", "Brown", "Mean", 0);
+
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive)
+VALUES
+("Melissa", 2, "F", "Brown", "Nice", 1);
+
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive)
+VALUES
+("Grinch", 30, "M", "Brown", "Mean", 0);
+
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive)
+VALUES
+("Wendy", 30, "M", "Brown", "Mean", 0);
+
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive)
+VALUES
+("Unnamed", 30, "M", "Brown", "Mean", 0);
+
+unnamed (refer back to how to create a record that doesn't have one value)
